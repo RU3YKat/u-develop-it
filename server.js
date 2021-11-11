c38d3eeba6d2e340d12b67e8b55067bc4625334e
insomniKat <ru3ykat@gmail.com>
2021-11-10 22:51:15 -0500

    connect db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -10,6 +11,23 @@ app.get('/', (req, res) => {
     res.json({
         message: 'Hello not Florida!'
     });
+});
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: 'ru3ykat-mySQL',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // Default response for any other request (Not Found)
